bd99c7134159bb0c9009b23d9abf0c06eade72f4
KarasuTatehi <45784893+KarasuTatehi@users.noreply.github.com>
2020-12-29 00:00:45 +0900

    Add youtube-chat script

--- src/extension/index.ts
+++ src/extension/index.ts
@@ -1,5 +1,16 @@
 import { NodeCG } from '../types/server';
 
+import { LiveChat } from 'youtube-chat';
+import { CommentItem } from 'youtube-chat/dist/parser';
+
 export = (nodecg: NodeCG) => {
+
+  const channelId = { channelId: 'UC_a1ZYZ8ZTXpjg9xUY9sj8w' };
+
+  const listener = (comment: CommentItem) => {
+    nodecg.sendMessage('comment', comment);
+  };
+
+  new LiveChat(channelId).on('comment', listener).start();
 
 };
